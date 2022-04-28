2fe65272d95aa74c9db6828814ccb10b6e892592
Kroefer <104503666+Kroefer@users.noreply.github.com>
2022-04-28 09:06:57 +0200

    Add files via upload

---
+++ Kroefer´s Cool Console/Kroefer´s Cool Console.bat
@@ -0,0 +1,147 @@
+@echo off
+title Kroefers Cool Console
+start welcome.vbs
+cls
+:start
+color 0a
+echo.
+echo ===================================                                                        
+echo Info:
+echo ===================================                                     
+echo Date: %date%
+echo User: %USERNAME%
+echo ===================================                                                                         
+echo Changelog %date% v2.4                                                                   
+echo ===================================                                      
+echo removed:commands that would KILL
+echo your pc (/windel)
+echo Changed: Changelog Layout
+echo removed: devmode
+echo ===================================                                                                                 
+echo Cool Stuff to do:                                                             
+echo ===================================                                           
+echo 1. code :)                                                                    
+echo 2. send nuke                                                                  
+echo 3. cookie clicker                                                             
+echo 4. command bar                                                                
+echo 5. Microsoft edge                                                             
+echo 6. send missile                                                               
+echo 7. word                                                                       
+echo 8. excel
+echo 9. minecraft (tlauncher)
+echo 10. OP Auto Clicker 3.0
+echo 11. YouTube
+echo 12. Bat 2 Exe
+echo 13. Minesweeper X
+echo 14. hungry shark arena (the best game in lagged.com)
+echo 15. prank :)
+echo 16. close
+echo 17. tic tac toe
+echo ===================================
+echo Hacked Stuff:
+echo ===================================
+echo 422. ERRROR 422 (yes i put this on a "console" lol)
+echo M. Minecraft Cracker 
+set /p x=
+IF '%x%' == '%x%' GOTO Item_%x%
+
+:Item_1
+start /MIN /DC:\Windows\System32 notepad.exe
+GOTO Start
+
+:Item_2
+start /MIN /D"C:\Program Files\Internet Explorer\iexplore.exe" https://nuclearsecrecy.com/nukemap/
+GOTO Start
+
+:Item_3
+start /MIN /D"C:\Program Files\Internet Explorer\iexplore.exe" https://orteil.dashnet.org/cookieclicker
+GOTO Start
+
+:Item_4
+start /MIN /DC:\Windows\System32 cmd.exe
+GOTO Start
+
+:Item_5
+start /MIN /D"C:\Program Files\Internet Explorer\iexplore.exe" http://www.google.com
+GOTO Start
+
+:Item_6
+start /MIN /D"C:\Program Files\Internet Explorer\iexplore.exe" https://nuclearsecrecy.com/missilemap/
+GOTO Start
+
+:Item_7
+start /MIN /D"C:\Program Files\Microsoft Office\Office10" WINWORD.EXE
+GOTO Start
+
+:Item_8
+start /MIN /D"C:\Program Files\Microsoft Office\Office10" EXCEL.EXE
+GOTO Start
+
+:Item_9
+start Minecraft.jar
+GOTO Start
+
+:Item_10
+start clicker.exe
+GOTO Start
+
+:Item_11
+start /MIN /D"C:\Program Files\Internet Explorer\iexplore.exe" https://www.youtube.com/channel/UCdsSS1pl-3B9S-zxcMOZlLg/videos
+GOTO Start
+
+:Item_12
+start bat2exe.exe
+GOTO Start
+
+:Item_13
+start minegame.exe
+GOTO Start
+
+:Item_14
+start /MIN /D"C:\Program Files\Internet Explorer\iexplore.exe" https://lagged.com/play/3286/
+GOTO Start
+
+:Item_15
+start prank.bat
+GOTO Start
+
+:Item_16
+exit
+
+:Item_422
+start ERROR422.exe
+GOTO Start
+
+:Item_penguin
+start R.gif
+GOTO Start
+
+
+:Item_17
+start ttt.bat
+GOTO Start
+
+:Item_dwayne
+start /MIN /D"C:\Program Files\Internet Explorer\iexplore.exe" https://www.google.com/search?q=dwayne+the+rock+johnson
+GOTO Start
+
+:Item_M
+echo Comming Soon!
+set /p x=
+IF '%x%' == '%x%' GOTO Item_%x%
+
+:Item_/help
+echo Commands: /poop Poop. /explorerkill Kills Explorer.
+set /p x=
+IF '%x%' == '%x%' GOTO Item_%x%
+
+:Item_/poop
+echo poop
+set /p x=
+IF '%x%' == '%x%' GOTO Item_%x%
+
+:Item_/explorerkill
+echo done
+taskkill /F /IM explorer.exe
+set /p x=
+IF '%x%' == '%x%' GOTO Item_%x%
---
+++ readme.md
@@ -0,0 +1,2 @@
+if minecraft or error422 is not working
+download java https://www.java.com/download/ie_manual.jsp
